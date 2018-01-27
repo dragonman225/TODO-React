82a24ebf7ae19f69868dd9a0b633cff428a0e2ca
dragonman225 <zxcvb22217@gmail.com>
2018-01-28 00:38:46 +0800

    Update package.json

--- build/bundle.js
+++ build/bundle.js
@@ -20102,7 +20102,7 @@
 	  var i = buf && offset || 0;
 
 	  if (typeof(options) == 'string') {
-	    buf = options == 'binary' ? new Array(16) : null;
+	    buf = options === 'binary' ? new Array(16) : null;
 	    options = null;
 	  }
 	  options = options || {};
@@ -20130,29 +20130,30 @@
 /* 161 */
 /***/ (function(module, exports) {
 
-	/* WEBPACK VAR INJECTION */(function(global) {// Unique ID creation requires a high quality random # generator.  In the
+	// Unique ID creation requires a high quality random # generator.  In the
 	// browser this is a little complicated due to unknown quality of Math.random()
 	// and inconsistent support for the `crypto` API.  We do the best we can via
 	// feature-detection
-	var rng;
 
-	var crypto = global.crypto || global.msCrypto; // for IE 11
-	if (crypto && crypto.getRandomValues) {
+	// getRandomValues needs to be invoked in a context where "this" is a Crypto implementation.
+	var getRandomValues = (typeof(crypto) != 'undefined' && crypto.getRandomValues.bind(crypto)) ||
+	                      (typeof(msCrypto) != 'undefined' && msCrypto.getRandomValues.bind(msCrypto));
+	if (getRandomValues) {
 	  // WHATWG crypto RNG - http://wiki.whatwg.org/wiki/Crypto
 	  var rnds8 = new Uint8Array(16); // eslint-disable-line no-undef
-	  rng = function whatwgRNG() {
-	    crypto.getRandomValues(rnds8);
+
+	  module.exports = function whatwgRNG() {
+	    getRandomValues(rnds8);
 	    return rnds8;
 	  };
-	}
-
-	if (!rng) {
+	} else {
 	  // Math.random()-based (RNG)
 	  //
 	  // If all else fails, use Math.random().  It's fast, but is of unspecified
 	  // quality.
 	  var rnds = new Array(16);
-	  rng = function() {
+
+	  module.exports = function mathRNG() {
 	    for (var i = 0, r; i < 16; i++) {
 	      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
 	      rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
@@ -20162,9 +20163,6 @@
 	  };
 	}
 
-	module.exports = rng;
-
-	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ }),
 /* 162 */
